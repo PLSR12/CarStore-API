3cf8a95ff596be54bca85627864c73445ff7f05f
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2025-05-22 16:40:26 -0300

    feat: create string extension

---
+++ src/BackEnd/CarStore.Domain/Extensions/StringExtension.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CarStore.Domain.Extensions
+{
+    public static class StringExtension
+    {
+        public static bool NotEmpty([NotNullWhen(true)] this string? value) => !string.IsNullOrEmpty(value);
+    }
+}
