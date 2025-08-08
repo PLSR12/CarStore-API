8dcaa16ed281416bc7d47352b4928d9883a7445e
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2025-08-08 17:16:09 -0300

    feat: add healtcheck service

---
+++ src/BackEnd/CarStore.API/Filters/HealthCheckDocumentFilter.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace CarStore.API.Filters
+{
+
+
+    public class HealthCheckDocumentFilter : IDocumentFilter
+    {
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            var pathItem = new OpenApiPathItem();
+            pathItem.AddOperation(OperationType.Get, new OpenApiOperation
+            {
+                Tags = new List<OpenApiTag> { new OpenApiTag { Name = "Health" } },
+                Responses = new OpenApiResponses
+            {
+                { "200", new OpenApiResponse { Description = "Serviço saudável" } },
+                { "503", new OpenApiResponse { Description = "Serviço com falhas" } }
+            }
+            });
+
+            swaggerDoc.Paths.Add("/health", pathItem);
+        }
+    }
+
+}
--- src/BackEnd/CarStore.API/Program.cs
+++ src/BackEnd/CarStore.API/Program.cs
@@ -5,71 +5,88 @@ using CarStore.Application;
 using CarStore.Domain.Security.Tokens;
 using CarStore.Infrastructure;
 using CarStore.Infrastructure.Migrations;
+using CarStore.Infrastrucutre.Redis;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
 
-builder
-    .Services.AddControllers()
-    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new StringConverter()));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions.Converters.Add(new StringConverter()));
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(options =>
 {
-    options.AddSecurityDefinition(
-        "Bearer",
-        new OpenApiSecurityScheme
-        {
-            Description =
-                @"JWT Authorization header using the Bearer scheme.
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme.
                         Enter 'Bearer' [space] and then your token in the text input below.
-                        Example:  'Bearer 123uSabcdef'",
-            Name = "Authorization",
-            In = ParameterLocation.Header,
-            Type = SecuritySchemeType.ApiKey,
-            Scheme = "Bearer",
-        }
-    );
+                        Example: 'Bearer 123uSabcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+    });
 
-    options.AddSecurityRequirement(
-        new OpenApiSecurityRequirement
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
         {
+            new OpenApiSecurityScheme
             {
-                new OpenApiSecurityScheme
+                Reference = new OpenApiReference
                 {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer",
-                    },
-                    Scheme = "oauth2",
-                    Name = "Bearer",
-                    In = ParameterLocation.Header,
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer",
                 },
-                new List<string>()
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
             },
+            new List<string>()
         }
-    );
+    });
 });
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    c.SwaggerDoc("v1", new OpenApiInfo
     {
         Title = "CarStore - API",
         Version = "v1",
         Description = "Documentação da API da CarStore.",
     });
+
+    c.DocumentFilter<HealthCheckDocumentFilter>();
 });
 
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
 builder.Services.AddApplications(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddInfraRedisServices(builder.Configuration);
 builder.Services.AddScoped<ITokenProvider, HttpContextTokenValue>();
+
+builder.Services.AddHealthChecks()
+    .AddMySql(
+        builder.Configuration.GetConnectionString("ConnectionMySql")!,
+        name: "mysql",
+        failureStatus: HealthStatus.Unhealthy)
+    .AddRedis(
+        builder.Configuration.GetConnectionString("ConnectionRedis")!,
+        name: "redis",
+        failureStatus: HealthStatus.Unhealthy);
+
+builder.Services.AddHealthChecksUI(setup =>
+{
+    setup.AddHealthCheckEndpoint("API Health", "/health");
+}).AddInMemoryStorage();
 
 builder.Services.AddHttpContextAccessor();
 
@@ -77,18 +94,20 @@ builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 var app = builder.Build();
 
+// Configure middleware
 
-app.UseSwaggerUI(c =>
-{
-    c.ConfigObject.AdditionalItems["persistAuthorization"] = true;
-});
-
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.ConfigObject.AdditionalItems["persistAuthorization"] = true;
+    });
+}
+else
+{
+    // Em produção, você pode querer também habilitar Swagger UI, dependendo do seu cenário.
+    // Remova ou mantenha conforme necessário.
 }
 
 app.UseHttpsRedirection();
@@ -97,19 +116,33 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-MigrateDataBase();
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+    ResultStatusCodes =
+    {
+        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+        [HealthStatus.Degraded] = StatusCodes.Status200OK,
+        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+    }
+});
+
+app.MapHealthChecksUI(options =>
+{
+    options.UIPath = "/health-ui";
+});
+
+MigrateDatabase();
 
 await app.RunAsync();
 
-void MigrateDataBase()
+void MigrateDatabase()
 {
     if (builder.Configuration.IsUnitTestEnviroment())
-    {
         return;
-    }
-    var connectionString = builder.Configuration.ConnectionString();
-    var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
 
+    var connectionString = builder.Configuration.ConnectionString();
+    using var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
     DatabaseMigration.Migrate(connectionString!, serviceScope.ServiceProvider);
 }
 
